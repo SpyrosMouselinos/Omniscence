ecf4de7f6d5eb741f85538c10f14f1b23407682d
Spyros Mouselinos <36852420+SpyrosMouselinos@users.noreply.github.com>
2018-12-28 18:39:06 +0200

    Update and rename __init__.py.py to __init__.py

--- Omniscence/__init__.py.py
+++
@@ -1,7 +0,0 @@
-# import numpy as np
-# import pandas as pd
-# import seaborn as sns
-# import matplotlib.pyplot as plt
-# import sklearn
-# from sklearn.mixture import BayesianGaussianMixture as BGM
-from Omniscence.Data_analysis import Omniscence
